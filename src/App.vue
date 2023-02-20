de5ed2c2ebf83ffa93724dd4ecf225eaabfb5d3a
sousa-p <3mail.al3atorio.wow@gmail.com>
2023-02-20 16:14:23 -0300

    Feature layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <nav>
+      <a href="#">NoVicious</a>
+      <router-link to="/">Home</router-link> |
+    </nav>
+  </header>
+  <main>
+    <SidebarComponent />
+    <router-view/>
+  </main>
+  <footer>
+    <p id="credito">Desenvolvido por Pedro S.</p>
+  </footer>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="sass">
+$header-footer-altura: 65px
+$black: #212227
+$shadow: 0px 0px 10px rgba(0,0,0,0.5)
 
-nav {
-  padding: 30px;
+*
+  padding: 0
+  margin: 0
+  box-sizing: border-box
+  color: $black
+  font-family: 'Courier New', Courier, monospace
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+#app
+  width: 100vw
+  height: 100vh
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+header, main, footer
+  width: 100%
+  display: flex
+
+header,footer
+  height: $header-footer-altura
+  align-items: center
+  padding: 0 25px
+  box-shadow: $shadow
+  background: #4f6367
+
+main
+  background: #eef5fb
+  height: calc(100% - $header-footer-altura*2)
+</style>
+
+<script>
+import SidebarComponent from '@/components/Sidebar.vue'
+export default {
+  components: {
+    SidebarComponent
   }
 }
-</style>
+</script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Sidebar">
+    <h1>Sidebar</h1>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.Sidebar
+  width: 20%
+  background: #7a9e9f
+</style>
+
+<script>
+export default {
+  name: 'SidebarComponent'
+}
+</script>
